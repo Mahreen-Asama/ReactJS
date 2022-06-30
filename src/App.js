077c943f366cc9a7c94e598c1b435f082aff4d9a
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-06-30 10:00:16 +0500

    function+class components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User'       // to use any component, we need to import it
+import {User2} from './User'
+import {User3} from './User'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <User/>
+      <User2/>
+      <User3/>
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback } from 'react';
 
-function App() {
+//class component
+import React,{Component} from 'react'
+
+var var1=20*2
+
+export class User3 extends Component
+{
+  render(){
+    return(
+      <div>class {var1} component</div>
+    )
+  }
+}
+
+function User() {
   return (
     <div className="App">
-      <h1>Hello users</h1>
+      <h1>users</h1>
     </div>
   );
 }
 
-export default App;
+// to use, we need to export
+export default User;
+
+export function User2() {
+  return (
+    <div className="App">
+      <h1>users 2</h1>
+    </div>
+  );
+}
