437ef77acc5017ba4cc41278179dba6707c94bde
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-06-30 10:54:06 +0500

    click event added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import {User2} from './User'
 import {User3} from './User'
 
 function App() {
+  // let name='Mahreen'
+
+  function apple(){
+    alert('function call');
+  }
   return (
     <div className="App">
       <h1>Hello world</h1>
       <User/>
       <User2/>
       <User3/>
+      <button onClick={()=>alert('direct alert')}>button 1</button>
+      <button onClick={()=>apple()}>button 2</button>
+      <button onClick={apple}>button 3</button>
     </div>
   );
 }
